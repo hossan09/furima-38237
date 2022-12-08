188f942b00f551e5838f7ae0725f5abf160cb1ea
hossan09 <hossan9.saito@gmail.com>
2022-12-09 07:36:13 +0900

    トークンが空では登録できないテストコードを追記

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :order_address do
     user_id {1}
     item_id {1}
+    token {"tok_abcdefghijk00000000000000000"}
     post_code {'123-4567'}
     area_id {15}
     municipalities {'横浜市'}
