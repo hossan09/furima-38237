c92a4d0588173096e268ed4d662061d94e63457b
hossan09 <hossan9.saito@gmail.com>
2022-12-10 18:52:37 +0900

    バリデーションにitem_idを追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,6 +6,7 @@ class OrderAddress
     validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :user_id
+    validates :item_id
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities
     validates :address
